a9eeee2c4748b85bbcabc222810a3b3668b1d801
Jeremy-Chen <jchen93@Hotmail.com>
2019-01-22 11:29:00 -0500

    Updates

---
+++ precache-manifest.806ee86d3566cd3ca5bb68445ce0cc91.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4765120232f515eba771",
+    "url": "/Crypto-Real-Time-/static/js/runtime~main.47651202.js"
+  },
+  {
+    "revision": "92568431f52608c86abc",
+    "url": "/Crypto-Real-Time-/static/js/main.92568431.chunk.js"
+  },
+  {
+    "revision": "e38b4f93d721fd86b8b3",
+    "url": "/Crypto-Real-Time-/static/js/1.e38b4f93.chunk.js"
+  },
+  {
+    "revision": "382f358171b05d5d6e6eaf92386733e3",
+    "url": "/Crypto-Real-Time-/index.html"
+  }
+];
